6393db134b760493f411dbd1a534ac9e5b28d256
Tneswick Laptop <tneswick2@verizon.net>
2024-01-09 07:00:39 -0800

    hamburger menu logic implemented, working on full styling of navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Photos from './components/Photos';
 import Rsvp from './components/RSVP';
 import About from './components/About';
 import NavBar from './components/NavBar';
+import './App.css';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,30 @@
 // Home.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/photos">Photos</Link>
-        <Link to="/rsvp">RSVP</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <div className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+        <button className="menu-button" onClick={toggleMenu}>
+        ☰
+        </button>
+        <h1 className='site-title'>
+          Neswick Wedding
+        </h1>
+        <nav className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <Link to="/">Home</Link>
+          <Link to="/photos">Photos</Link>
+          <Link to="/rsvp">RSVP</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </div>
     </div>
   );
 }
