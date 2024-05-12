7b7817ef25496d618f046f78f3f69681569374e2
Deck Stream <tneswick2@verizon.net>
2024-05-11 21:43:47 -0700

    imgs fixed. about section reorganized

--- client/src/components/Photos.js
+++ client/src/components/Photos.js
@@ -3,7 +3,7 @@ import './Photos.css';
 
 const Photos = () => {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const numPhotos = 20; // Total number of photos
+  const numPhotos = 18; // Total number of photos
 
   const handleClick = (photo) => {
     setSelectedPhoto(photo);
